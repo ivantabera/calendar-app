e82403733384a8c1ddfb9742243cceaa2a5a63b9
Ivan Tabera <ivantabera19@gmail.com>
2021-11-02 21:23:38 -0600

    React big calendar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { CalendarApp } from './CalendarApp';
 
+import './styles.css';
+
 ReactDOM.render(
     <CalendarApp />,
   document.getElementById('root')
