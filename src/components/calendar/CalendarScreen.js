42794906f4bac068d52964efe82334e4ee2286b6
Ivan Tabera <ivantabera19@gmail.com>
2021-11-04 15:06:12 -0600

    Configuraciones adicionales al big Calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -3,18 +3,46 @@ import {Calendar, momentLocalizer} from 'react-big-calendar';
 import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
+
+/* Se utiliza messajes para cambiar los mensajes del big calendar */
+import { messages } from '../../helpers/calendar-messages-es';
+
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+/* Se utiliza para cambiar el idioma del big calendar */
+import 'moment/locale/es';
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
+/* Estructura del arreglo de objetos para los eventos del big calendar*/
 const events = [{
     title: 'Cumpleaños ivan',
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: 'Comprar el pastel'
 }]
 
 export const CalendarScreen = () => {
+
+    /* Esta funcion se va disparar con ciertos argumentos gracias al componente Calendar */
+    const eventStylerGetter = ( event, start, end, isSelected ) => {
+        
+        const style = {
+            backgroundColor: '#367cf7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        /* Lo que regresa esta funcion es el estilo que le va aplicar al evento */
+        return {
+            style
+        }
+
+    };
+
     return (
         <div className="calendar-screen">
             <Navbar />
@@ -24,6 +52,8 @@ export const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
+                messages={messages}
+                eventPropGetter={ eventStylerGetter }
             />
         </div>
     )
