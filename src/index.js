66008fdf158cdd8bc322d49de9edf4262fd44c24
Ivan Tabera <ivantabera19@gmail.com>
2021-12-02 21:07:53 -0600

    Se crearon las variables de entorno

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { CalendarApp } from './CalendarApp';
 
 import './styles.css';
 
+console.log('process.env', process.env)
+
 ReactDOM.render(
     <CalendarApp />,
   document.getElementById('root')
