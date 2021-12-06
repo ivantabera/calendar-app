f1fc3f5d43e94d97ccfc3e7c4162080021511fb2
Ivan Tabera <ivantabera19@gmail.com>
2021-12-06 15:20:33 -0600

    se quitaron las consolas por seguridad

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,10 +1,8 @@
 const baseUrl = process.env.REACT_APP_API_URL;
 
 const fetchSinToken = ( endpoint, data, method = 'GET' ) => {
-    console.log('data', data)
 
     const url = `${baseUrl}/${endpoint}`;
-    console.log('url', url)
 
     if ( method === 'GET' ) {
 
