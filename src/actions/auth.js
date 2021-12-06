6ddf7486a4cb928caa94dd7195ea75e62ed54a19
Ivan Tabera <ivantabera19@gmail.com>
2021-12-06 15:29:07 -0600

    Manejo del error en usuario

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 import { fetchSinToken } from "../helpers/fetch"
 import { types } from "../types/types";
 
@@ -21,6 +22,8 @@ export const startLogin = ( email, password ) => {
                 name:body.name
             }) );
 
+        } else {
+            Swal.fire('Error', body.msg, 'error');
         }
     }
 
