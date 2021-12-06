921f8c834e848d3801ab23a8e763cfeacc063f7d
Ivan Tabera <ivantabera19@gmail.com>
2021-12-06 15:19:15 -0600

    Realizar peticion http para autenticarnos

---
+++ src/helpers/fetch.js
@@ -0,0 +1,31 @@
+const baseUrl = process.env.REACT_APP_API_URL;
+
+const fetchSinToken = ( endpoint, data, method = 'GET' ) => {
+    console.log('data', data)
+
+    const url = `${baseUrl}/${endpoint}`;
+    console.log('url', url)
+
+    if ( method === 'GET' ) {
+
+        return fetch( url )
+
+    } else{
+
+        return fetch( 
+            url, 
+            {
+                method,
+                headers:{
+                    'Content-type':'application/json'
+                },
+                body:JSON.stringify( data )
+            }
+        )
+        
+    }
+}
+
+module.exports = {
+    fetchSinToken
+}
