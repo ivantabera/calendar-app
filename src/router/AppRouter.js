004a666b3942e382846b75cbfc430eb3a7924015
Ivan Tabera <ivantabera19@gmail.com>
2021-12-08 11:54:48 -0600

    Proteccion de rutas

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,31 +1,51 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
 BrowserRouter as Router,
 Switch,
-Route,
 Redirect
 } from "react-router-dom";
 import { startChecking } from '../actions/auth';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 
     const dispatch = useDispatch();
 
+    /* Seleccionar del state el checking para validar si el usuario esta autenticado */
+    const { checking, uid } = useSelector(state => state.auth);
+
     /* Al usar este Hook, le estamos indicando a React que el componente 
     tiene que hacer el dispatch de renovar el token después de renderizarse. */
     useEffect(() => {
         dispatch( startChecking() );
-    }, [dispatch])
+    }, [dispatch]);
+
+    if (checking) {
+        return(<h1>Espere</h1>)
+    }
 
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={ LoginScreen } />
-                    <Route exact path="/" component={ CalendarScreen } />
+                    
+                    <PublicRoute 
+                        exact 
+                        path="/login" 
+                        component={ LoginScreen }
+                        isAuthenticated={ !!uid }
+                    />
+                    
+                    <PrivateRoute 
+                        exact 
+                        path="/" 
+                        component={ CalendarScreen } 
+                        isAuthenticated={ !!uid }
+                    />
 
                     <Redirect to="/" />
                 </Switch>
